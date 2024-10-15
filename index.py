9cb53d954906e11a18bc38f673decf2a337d50c8
Mitzunie <47071090+Mitzunie@users.noreply.github.com>
2024-10-15 16:02:52 -0300

    Update index.py

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import requests
 import selenium
 import pandas as pd
 import datetime
+from termcolor import colored
 
 def opt():
     while True:
@@ -10,7 +11,9 @@ def opt():
             opcion = int(opcion)
             return opcion
         else:
-            print("\n----------========== Advertencia ==========----------\n> Opción Invalida, Vuelve a intentarlo...\n----------=================================----------")
+            print(colored("\n----------========== Advertencia ==========----------\n", "red"))
+            print(colored(f"> La opción ingresada [{opcion}] es invalida.", "yellow"))
+            print(colored("\n----------=================================----------\n", "red"))
         return    
 def tarea1():
     return
@@ -61,59 +64,58 @@ url = "https://www.sismologia.cl/sismicidad/catalogo/2024/09/20240920.html"
 
 respuesta = requests.get(url)
 
-print("\n------====== Verificación de Conexión ======------\n\nIniciando Conexión...\n")
+print(colored("\n----------========== Verificando Conexión ==========----------\n","light_yellow"))
 if respuesta.status_code == 200:
-    print("Conexión Exitosa...")
+    print(colored("> Conexión Establecida Exitosamente!\n","green"))
+    print(colored("----------========================================----------\n","light_yellow"))
+
+    fecha_actual = datetime.datetime.today().strftime('%d/%m/%Y')
+
+    while True:
+        print("¡Bienvenido(a)!, ¿Qué deseas hacer?")
+        print("1.- Mostrar Ranking de 3 Sismos con Mayor Magnitud")
+        print(f"2.- Mostrar Profundidad Acumulada De Todos los Sismos del Día({fecha_actual}).")
+        print("3.- Mostrar Sismo De Menor Magnitud")
+        print("4.- Mostrar Sismos Ocurridos En Un Intervalo De Tiempo.")
+        print("5.- Mostrar Datos Del Ultimo Sismo.")
+        print("6.- Mostrar Tiempo Transcurrido Desde El Ultimo Sismo")
+        print("7.- Enviar Información Del Ultimo Sismo Al Correo")
+        print("8.- Mostrar Grafico Intensidad de Sismos")
+        print("9.- Mostrar Grafico Profundidad")
+        print("10.- Grafico Comparativo Sismos")
+        print("0.- Salir del Programa")
+    
+        opcion = opt()
+    
+        if opcion == 1:
+            tarea1()
+        elif opcion == 2:
+            tarea2()
+        elif opcion == 3:
+            tarea3()
+        elif opcion == 4:
+            tarea4()
+        elif opcion == 5:
+            tarea5()
+        elif opcion == 6:
+            tarea6()
+        elif opcion == 7:
+            tarea7()
+        elif opcion == 8:
+            tarea8()
+        elif opcion == 9:
+            tarea9()
+        elif opcion == 10:
+            tarea10()
+        elif opcion == 0:
+            print(colored("\n> Finalizando programa, ¡Hasta la Proxima!\n","red"))
+            break
+            
 elif respuesta.status_code == 404:
-    print(f"Conexión Fallida, No se encuentra la página que buscas...\nCODE: {respuesta.status_code}")
+    print(colored(f"Conexión Fallida, No se encuentra la página que buscas...\nCODE: {respuesta.status_code}", "red"))
 elif respuesta.status_code == 204:
-    print(f"Conexión Exitosa, Lamentablemente no se encuentran datos para devolver.\nCODE: {respuesta.status_code}")
+    print(colored(f"Conexión Exitosa, Lamentablemente no se encuentran datos para devolver.\nCODE: {respuesta.status_code}", "yellow"))
 elif respuesta.status_code == 403:
-    print(f"Conexión Fallida (FORBIDDEN), El servidor detecta la petición pero el servidor no cumple con esta.\nCODE: {respuesta.status_code}")
+    print(colored(f"Conexión Fallida (FORBIDDEN), El servidor detecta la petición pero el servidor no cumple con esta.\nCODE: {respuesta.status_code}", "red"))
 else: 
-    print(f"Conexión Fallida: {respuesta.status_code}.")
-print("\n------======================================------\n")
-
-fecha_actual = datetime.datetime.today().strftime('%d/%m/%Y')
-
-while True:
-    print("¡Bienvenido(a)!, ¿Qué deseas hacer?")
-    print("1.- Mostrar Ranking de 3 Sismos con Mayor Magnitud")
-    print(f"2.- Mostrar Profundidad Acumulada De Todos los Sismos del Día({fecha_actual}).")
-    print("3.- Mostrar Sismo De Menor Magnitud")
-    print("4.- Mostrar Sismos Ocurridos En Un Intervalo De Tiempo.")
-    print("5.- Mostrar Datos Del Ultimo Sismo.")
-    print("6.- Mostrar Tiempo Transcurrido Desde El Ultimo Sismo")
-    print("7.- Enviar Información Del Ultimo Sismo Al Correo")
-    print("8.- Mostrar Grafico Intensidad de Sismos")
-    print("9.- Mostrar Grafico Profundidad")
-    print("10.- Grafico Comparativo Sismos")
-    print("0.- Salir del Programa")
-    
-    opcion = opt()
-    
-    if opcion == 1:
-        tarea1()
-    elif opcion == 2:
-        tarea2()
-    elif opcion == 3:
-        tarea3()
-    elif opcion == 4:
-        tarea4()
-    elif opcion == 5:
-        tarea5()
-    elif opcion == 6:
-        tarea6()
-    elif opcion == 7:
-        tarea7()
-    elif opcion == 8:
-        tarea8()
-    elif opcion == 9:
-        tarea9()
-    elif opcion == 10:
-        tarea10()
-    elif opcion == 0:
-        print("\nFinalizando Programa, ¡Adiós!\n")
-        break
-    else:
-        print("\n> La opción Ingresada es INVALIDA, Reintentalo Nuevamente...\n")
+    print(colored(f"Conexión Fallida: {respuesta.status_code}.", "red"))
