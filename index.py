7bd448f866d3f931ee001a4bf6fda75d05e23260
Mitzunie <47071090+Mitzunie@users.noreply.github.com>
2024-10-18 18:04:18 -0300

    Update index.py

--- index.py
+++ index.py
@@ -130,7 +130,68 @@ def tarea2():
     return
                 
 def tarea3():
+    Year = datetime.datetime.today().strftime("%Y")
+    Month = datetime.datetime.today().strftime("%m")
+    CompleteDate = datetime.datetime.today().strftime("%Y%m%d")
+    today_url = f"https://www.sismologia.cl/sismicidad/catalogo/{Year}/{Month}/{CompleteDate}.html"
+    status = requests.get(today_url)
+    print(colored("\n> Verificando URL...\n", "yellow"))
+    if status.status_code == 200:
+        print(colored("> URL Valida!", "green"))
+        
+        service = Service(executable_path="driver/chromedriver.exe")
+        driver = webdriver.Chrome(service=service)
+        
+        try:
+            driver.get(today_url)
+            time.sleep(3)
+            
+            tabla_sismos = driver.find_element(By.CSS_SELECTOR, ".sismologia.detalle")
+            filas = tabla_sismos.find_elements(By.TAG_NAME, 'tr')[1:]
+            
+            sismos = []
+            
+            for fila in filas:
+                celdas = fila.find_elements(By.TAG_NAME, 'td')
+                if celdas:
+                    magnitud_str = celdas[4].text.strip()
+                    profundidad_str = celdas[3].text.strip()
+                    ubicacion_str = celdas[0].text.strip()  
+                    fecha_str = celdas[1].text.strip()  
+                    
+                    try:
+                        magnitud_str = magnitud_str.replace("Ml", "")
+                        magnitud_str = magnitud_str.replace(" ", "")
+                        magnitud = float(magnitud_str)
+                        profundidad_str = profundidad_str.replace("km", "")
+                        profundidad_str = profundidad_str.replace(" ", "")
+                        profundidad = int(profundidad_str)
+                        fecha_str, ubicacion = ubicacion_str.split("\n")
+                    
+                        sismo = {
+                            'magnitud': magnitud,
+                            'profundidad': profundidad,
+                            'ubicacion': ubicacion,
+                            'fecha': fecha_str
+                        }
+                        sismos.append(sismo)
+                    except ValueError:
+                        print(colored(f">> Error: No se pudo convertir la magnitud o profundidad: {magnitud_str} {profundidad_str}", "red"))
+            
+            sismos_ordenados = sorted(sismos, key=lambda x: x['magnitud'], reverse=True)
+            
+            print(colored("\n> Sismo de Menor Magnitud: \n", "green"))
+            for i, sismo in enumerate(sismos_ordenados[-1:]):
+                print(colored(f"{i + 1}. Fecha y Hora: {sismo['fecha']} | Ubicación: {sismo['ubicacion']} | Magnitud: {sismo['magnitud']} Ml | Profundidad: {sismo['profundidad']} Km\n", "yellow"))
+            
+        finally:
+            driver.quit()
+
+    else:
+        print(colored("> Error: No se pudo acceder a la URL", "red"))
+            
     return
+
 def tarea4():
     print("\n> Debes ingresar el intervalo de tiempo para buscar los sismos.\n")
     print("\n>> Fecha de Inicio Intervalo <<\n")
@@ -183,16 +244,16 @@ if respuesta.status_code == 200:
 
     while True:
         print("¡Bienvenido(a)!, ¿Qué deseas hacer?")
-        print(f"1.- Mostrar Ranking de 3 Sismos con Mayor Magnitud")
+        print("1.- Mostrar Ranking de 3 Sismos con Mayor Magnitud")
         print(f"2.- Mostrar Profundidad Acumulada De Todos los Sismos del Día({fecha_actual})")
-        print(f"3.- Mostrar Sismo De Menor Magnitud")
+        print("3.- Mostrar Sismo De Menor Magnitud")
         print("4.- Mostrar Sismos Ocurridos En Un Intervalo De Tiempo.")
-        print(f"5.- Mostrar Datos Del Ultimo Sismo.")
-        print(f"6.- Mostrar Tiempo Transcurrido Desde El Ultimo Sismo")
+        print("5.- Mostrar Datos Del Ultimo Sismo.")
+        print("6.- Mostrar Tiempo Transcurrido Desde El Ultimo Sismo")
         print("7.- Enviar Información Del Ultimo Sismo Al Correo")
-        print(f"8.- Mostrar Grafico Intensidad de Sismos")
-        print(f"9.- Mostrar Grafico Profundidad")
-        print(f"10.- Grafico Comparativo Sismos")
+        print("8.- Mostrar Grafico Intensidad de Sismos")
+        print("9.- Mostrar Grafico Profundidad")
+        print("10.- Grafico Comparativo Sismos")
         print("0.- Salir del Programa")
     
         opcion = opt()
